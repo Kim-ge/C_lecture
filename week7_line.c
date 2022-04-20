050a59199ed4242284a64c277cbad3dc8590cc99
ddb <81467937+Kim-ge@users.noreply.github.com>
2022-04-20 13:53:51 +0900

    Create week7_line.c

---
+++ week7_line.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define SIZE 8
+
+int main(void) {
+	int i, k, tmp;
+	int list[SIZE]={10, 5, 8, 9, 6, 4, 1, 2 };
+
+	for (k = 0; k < SIZE; k++) {
+		for (i = 0; i < SIZE-1; i++) {
+			if (list[i] > list[i + 1]) {
+				tmp = list[i];
+				list[i] = list[i + 1];
+				list[i + 1] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < SIZE; i++) {
+		printf("%d", list[i]);
+	}
+
+	return 0;
+}
